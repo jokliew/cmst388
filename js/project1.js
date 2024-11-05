39232070e08167b815ef037cf7f219419409dfec
jokliew <joselle.o123@gmail.com>
2024-11-05 17:57:46 -0500

    part 2 1.5 project1.js

--- js/project1.js
+++ js/project1.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelectorAll('.selector-examples li:nth-child(odd)').forEach(el => el.classList.add('odd'));
         // TODO: Add remaining selectors using nth-child, loops and conditional logic where approriate. (https://www.w3schools.com/CSSref/sel_nth-child.php) 
         document.querySelectorAll('.selector-examples li:nth-child(even)').forEach(el => el.classList.add('even'));
-        
+        document.querySelector('.selector-examples li:last-child').classList.add('last');
     });
 
     /* PART 3: REPLACEMENT TEXT
