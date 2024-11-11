cf4a408717e0acac0d153fee23760b6c9bd3dedf
jokliew <joselle.o123@gmail.com>
2024-11-11 18:53:46 -0500

    Part 3 1.0 Project1.js

--- js/project1.js
+++ js/project1.js
@@ -60,6 +60,9 @@ document.addEventListener('DOMContentLoaded', () => {
         // - 1: Create a new variable called inputValue and set it to the value of the #newLanguage id
         // - 2: Create a new variable called element and set it to the .currentLanguage class
         // - 3: Using .innerHTML (https://www.w3schools.com/jsref/prop_html_innerhtml.asp), update element with inputValue
+        let inputValue = document.getElementById('newLanguage').value;
+        let element = document.querySelector('.currentLanguage');
+        element.innerHTML = inputValue;
     });
 
     /* PART 4: TOGGLES
